7f68c339676c32b44ed277482df36d6a3e8f5414
Patrick <pamarx92@gmail.com>
2022-07-16 14:10:59 +0200

    Update js.js
    fine tuning

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -6,6 +6,8 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const gameStatus = document.getElementById("gameStatus");
+const pScore = document.getElementById("playerScore");
+const cScore = document.getElementById("computerScore");
 
 let playerScore = 0;
 let computerScore = 0;
@@ -28,7 +30,7 @@ function playerChoice(playerInput) {
 function playRound(playerInput) {
 
   let playerSelection = playerChoice(playerInput);
-  computerSelection = computerPlay();
+      computerSelection = computerPlay();
 
   if (playerSelection === computerSelection) {
     gameStatus.innerHTML = "Game: " + "Tie!";
@@ -37,25 +39,25 @@ function playRound(playerInput) {
     playerScore++;
     round++;
     gameStatus.innerHTML = "Game: " + "You Win! Rock beats Scissors.";
-    document.getElementById("playerScore").textContent = playerScore;
+    pScore.textContent = playerScore;
   } else if (playerSelection === items[1] && computerSelection === items[0]) {
     playerScore++;
     round++;
     gameStatus.innerHTML = "Game: " + "You Win! Paper beats Rock.";
-    document.getElementById("playerScore").textContent = playerScore;
+    pScore.textContent = playerScore;
   } else if (playerSelection === items[2] && computerSelection === items[1]) {
     playerScore++;
     round++;
     gameStatus.innerHTML = "Game: " + "You Win! Scissors beat Paper.";
-    document.getElementById("playerScore").textContent = playerScore;
+    pScore.textContent = playerScore;
   } else {
     computerScore++;
     round++;
     gameStatus.innerHTML = "Game: " + "You Lose!";
-    document.getElementById("computerScore").textContent = computerScore;
+    cScore.textContent = computerScore;
   }
-  document.getElementById("player").innerHTML = "Player: " + playerSelection;
-  document.getElementById("computer").innerHTML = "Computer: " + computerSelection;
+  document.getElementById("player").innerHTML = "Player choose: " + playerSelection;
+  document.getElementById("computer").innerHTML = "Computer choose: " + computerSelection;
   document.getElementById("round").innerHTML = round;
 }
 
@@ -75,8 +77,8 @@ function restart() {
   playerScore = 0;
   computerScore = 0;
   round = 0;
-  document.getElementById("playerScore").textContent = 0;
-  document.getElementById("computerScore").textContent = 0;
+  pScore.textContent = 0;
+  cScore.textContent = 0;
   document.getElementById("round").innerHTML = 0;
   winOrLose.innerHTML = "Rock, Paper, Scissors";
   winOrLose.classList.remove("win");
